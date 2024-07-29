5a279d2bb3812f4b55cc563df73a4fee161a16ce
Ganesh <wganeshb@gmail.com>
2024-07-29 23:13:42 +0530

    [Patch] Change Subscribe button title (was Subscribe now)

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Swiftful!")
             
-            Button("Subscribe now!") {
+            Button("Subscribe!") {
                 
             }
 
