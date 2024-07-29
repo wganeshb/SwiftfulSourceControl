b1c0c81aee8af52d703f955d97e264538aa7c669
Ganesh <wganeshb@gmail.com>
2024-07-30 00:02:31 +0530

    [Patch] Fix subscribe button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking!")
             
-            Button("Subscribe!") {
+            Button("Subscribe") {
                 
             }
 
