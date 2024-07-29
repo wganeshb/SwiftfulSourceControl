e9fda9f94d440b5d9be36a7bc29e7183f46ebed0
Ganesh <wganeshb@gmail.com>
2024-07-29 07:16:57 +0530

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Ganesh Balasaheb Waghmode on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
