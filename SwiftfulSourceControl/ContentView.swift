40e4000cea51bbbaca2a5327c629016cb7ecf7d8
Ganesh <wganeshb@gmail.com>
2024-07-30 01:46:15 +0530

    [Feature] Change heart icon to bolt

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "heart.fill")
+            Image(systemName: "bolt.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking!")
