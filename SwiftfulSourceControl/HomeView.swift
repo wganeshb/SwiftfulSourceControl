87b3afd70d1a4873edb3458647baccc85d6d33ec
Ganesh <wganeshb@gmail.com>
2024-07-29 08:20:26 +0530

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Ganesh Balasaheb Waghmode on 29/07/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
